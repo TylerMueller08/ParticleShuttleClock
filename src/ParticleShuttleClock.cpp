91a320c6b77225a1e9efd8b610f1d26668c603f4
TylerMueller08 <tylerjacobmueller08@gmail.com>
2024-01-14 23:41:02 -0700

    Simplification :)

--- src/ParticleShuttleClock.cpp
+++ src/ParticleShuttleClock.cpp
@@ -1,56 +1,124 @@
 /* 
  * Project: Particle Shuttle Clock
  * Author: Tyler Mueller
- * Date: 12/16/2023
+ * Last Updated: 1/14/2024
 */
 
+
+// Import Libraries & Define TFT Pins
 #include <Particle.h>
 #include "../lib/Adafruit_ILI9341/src/Adafruit_ILI9341.h"
-
 #if defined(PARTICLE)
   #define TFT_DC   D5
   #define TFT_CS   D4
 #endif
 
-// Define Variables
+
+// Instance Variables
 Adafruit_ILI9341 display = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
-String getCurrentWeekDay(int offset);
-String getAbbreviatedWeekDay(int offset);
-String getCurrentDate();
-String getCurrentTime();
-
+String yesterdayTemperature, todayTemperature, tomorrowTemperature, yesterdayCode, todayCode, tomorrowCode;
 extern uint8_t blank[], sunny[], cloudy[], fog[], rain[], snow[], thunderstorm[];
-String yesterdayTemp, todayTemp, tomorrowTemp;
-String yesterdayCode, todayCode, tomorrowCode;
-uint8_t* getWeatherCode(String weatherCode);
-void updateTemps();
-
-int lastMinute = -1;
-int lastHour = -1;
-int lastDay = -1;
+int lastMinute, lastHour, lastDay = -1;
 int dayOfWeek = 0;
 
-int centerTextX(String text, int textSize);
+uint8_t* getWeatherCode(String weatherCode) {
+  if (weatherCode == "1000" || weatherCode == "1100") return sunny;
+  if (weatherCode == "1101" || weatherCode == "1102" || weatherCode == "1001") return cloudy;
+  if (weatherCode == "2000" || weatherCode == "2100") return fog;
+  if (weatherCode == "4000" || weatherCode == "4001" || weatherCode == "4200" || weatherCode == "4201") return rain;
+  if (weatherCode == "5000" || weatherCode == "5001" || weatherCode == "5100" || weatherCode == "5101" || weatherCode == "6000" || weatherCode == "6001" || weatherCode == "6200" || weatherCode == "6201" || weatherCode == "7000" || weatherCode == "7101" || weatherCode == "7102") return snow;
+  if (weatherCode == "8000") return thunderstorm;
+  return blank;
+}
+void centerText(String text, int textSize, int yPos, int offset) {
+  int textWidth = text.length() * (textSize * 6);
+  int screenWidth = display.width();
+  int centeredX = (screenWidth - textWidth) / 2;
+  display.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  display.setCursor(centeredX + offset, yPos);
+  display.setTextSize(textSize);
+  display.print(text);
+}
+void screenLayout() {
+  display.fillScreen(ILI9341_BLACK);
 
-// Retrieve Temperature
-void temperatureHandler(const char *event, const char *tempData) {
+  display.drawRect(0, 0, 240, 320, ILI9341_WHITE);
+  display.drawFastHLine(10, 80, 230-10, ILI9341_WHITE);
+  display.drawFastHLine(10, 155, 230-10, ILI9341_WHITE);
+
+  centerText("Rapid City, South Dakota", 1, 300, 0);
+}
+String getCurrentWeekDay(int offset) {
+  dayOfWeek = (Time.weekday() + offset - 1 + 7) % 7 + 1;
+
+  switch (dayOfWeek) {
+    case 1: return "Sunday";
+    case 2: return "Monday";
+    case 3: return "Tuesday";
+    case 4: return "Wednesday";
+    case 5: return "Thursday";
+    case 6: return "Friday";
+    case 7: return "Saturday";
+    default: return "N/A";
+  }
+}
+String getCurrentDate() {
+  return Time.format("%B %e, %Y");
+}
+String getCurrentTime() {
+  return Time.format("%I:%M %p");
+}
+String getAbbreviatedWeekDay(int offset) {
+  String fullDay = getCurrentWeekDay(offset);
+  return fullDay.substring(0, 3);
+}
+void drawTempSymbols(int xPos, String temperature) {
+  int offset;
+
+  if (temperature.length() == 3) {
+    offset = 8;
+  } else if (temperature.length() == 1) {
+    offset = -5;
+  } else {
+    offset = 0;
+  }
+
+  display.setTextColor(ILI9341_LIGHTGREY, ILI9341_BLACK);
+  display.setTextSize(1);
+  display.setCursor(xPos + offset, 260);
+  display.print("o");
+  display.setCursor(xPos + offset, 272);
+  display.print("F");
+}
+void updateTemps() {
+  centerText(getAbbreviatedWeekDay(-1), 2, 170, -75);
+  centerText("Today", 2, 170, 0);
+  centerText(getAbbreviatedWeekDay(1), 2, 170, 75);
+
+  centerText(yesterdayTemperature, 2, 265, -75);
+  centerText(todayTemperature, 2, 265, 0);
+  centerText(tomorrowTemperature, 2, 265, 75);
+
+  drawTempSymbols(60, String(yesterdayTemperature));
+  drawTempSymbols(135, String(todayTemperature));
+  drawTempSymbols(210, String(tomorrowTemperature));
+}
+void temperatureHandler(const char *event, const char *data) {
   int index0, index1, index2;
-  String tempString = String(tempData);
+  String tempString = String(data);
 
   index0 = tempString.indexOf('~');
-  yesterdayTemp = String(round(tempString.substring(0, index0).toFloat())).toInt();
+  yesterdayTemperature = String(round(tempString.substring(0, index0).toFloat())).toInt();
 
   index1 = tempString.indexOf('~', index0+1);
-  todayTemp = String(round(tempString.substring(index0+1, index1).toFloat())).toInt();
+  todayTemperature = String(round(tempString.substring(index0+1, index1).toFloat())).toInt();
 
   index2 = tempString.indexOf('~', index1+1);
-  tomorrowTemp = String(round(tempString.substring(index1+1, index2).toFloat())).toInt();
+  tomorrowTemperature = String(round(tempString.substring(index1+1, index2).toFloat())).toInt();
 
   updateTemps();
 }
-
-// Retrieve WeatherCodeDay
 void weatherCodeHandler(const char *event, const char *weatherCodeData) {
   int index0, index1, index2;
   String codeString = String(weatherCodeData);
@@ -63,49 +131,32 @@ void weatherCodeHandler(const char *event, const char *weatherCodeData) {
 
   index2 = codeString.indexOf('~', index1+1);
   tomorrowCode = codeString.substring(index1+1, index2);
-  
-  display.fillRect(15, 192, 60, 60, ILI9341_BLACK);
-  display.fillRect(90, 192, 60, 60, ILI9341_BLACK);
-  display.fillRect(165, 192, 60, 60, ILI9341_BLACK);
 
   display.drawBitmap(15, 192, getWeatherCode(yesterdayCode), 60, 60, ILI9341_WHITE);
   display.drawBitmap(90, 192, getWeatherCode(todayCode), 60, 60, ILI9341_WHITE);
   display.drawBitmap(165, 192, getWeatherCode(tomorrowCode), 60, 60, ILI9341_WHITE);
 }
+void callWebhooks() {
+  String data = String(10);
+  Particle.publish("getTemperatures", data, PRIVATE);
+  Particle.publish("weatherCode", data, PRIVATE);
+}
 
 void setup() {
-  Particle.subscribe("hook-response/getTemp", temperatureHandler, MY_DEVICES);
-  Particle.subscribe("hook-response/weatherCode", weatherCodeHandler, MY_DEVICES);
-
   display.begin();
 
-  display.fillScreen(ILI9341_BLACK);
-  display.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  screenLayout();
 
-  display.drawRect(0, 0, 240, 320, ILI9341_WHITE);
-  display.drawFastHLine(10, 80, 230 - 10, ILI9341_WHITE);
-  display.drawFastHLine(10, 155, 230 - 10, ILI9341_WHITE);
+  Particle.subscribe("hook-response/getTemperatures", temperatureHandler, MY_DEVICES);
+  Particle.subscribe("hook-response/weatherCode", weatherCodeHandler, MY_DEVICES);
 
-  display.setTextSize(2);
-  display.setCursor(28, 170); display.print(getAbbreviatedWeekDay(-1));
-  display.setCursor(90, 170); display.print("Today");
-  display.setCursor(178, 170); display.print(getAbbreviatedWeekDay(1));
-  
-  int centerX = centerTextX("Rapid City, South Dakota", 1);
-  display.setCursor(centerX, 300);
-  display.setTextSize(1);
-  display.print("Rapid City, South Dakota");
-
-  String data = String(10);
-  Particle.publish("getTemp", data, PRIVATE);
-  Particle.publish("weatherCode", data, PRIVATE);
-
-  updateTemps();
+  callWebhooks();
 }
 
 void loop() {
   Time.zone(-7);
 
+  int currentSecond = Time.second();
   int currentMinute = Time.minute();
   int currentHour = Time.hour();
   int currentDay = Time.day();
@@ -113,133 +164,17 @@ void loop() {
   if (currentDay != lastDay) {
     lastDay = currentDay;
 
-    display.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
-
-    int centerX = centerTextX(getCurrentWeekDay(0), 3);
-    display.setCursor(centerX, 15);
-    display.setTextSize(3);
-    display.print(getCurrentWeekDay(0));
-
-    centerX = centerTextX(getCurrentDate(), 2);
-    display.setCursor (centerX, 50);
-    display.setTextSize(2);
-    display.print(getCurrentDate());
+    centerText(getCurrentWeekDay(0), 3, 15, 0);
+    centerText(getCurrentDate(), 2, 50, 0);
   }
-
   if (currentMinute != lastMinute || currentHour != lastHour) {
     lastMinute = currentMinute;
     lastHour = currentHour;
 
-    display.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
-
-    int centerX = centerTextX(getCurrentTime(), 4);
-    display.setCursor(centerX, 105);
-    display.setTextWrap(false);
-    display.setTextSize(4);
-    display.print(getCurrentTime());
+    centerText(getCurrentTime(), 4, 105, 0);
+  }
+  if (currentMinute == 0 && currentSecond == 0) {
+    callWebhooks();
   }
   delay(1000);
-}
-
-String getCurrentTime() {
-  Time.zone(-7);
-  return Time.format("%I:%M %p");
-}
-
-String getCurrentDate() {
-  Time.zone(-7);
-  return Time.format("%B %e, %Y");
-}
-
-String getCurrentWeekDay(int offset) {
-  Time.zone(-7);
-  dayOfWeek = (Time.weekday() + offset - 1 + 7) % 7 + 1;
-
-  switch (dayOfWeek) {
-    case 1: return "Sunday";
-    case 2: return "Monday";
-    case 3: return "Tuesday";
-    case 4: return "Wednesday";
-    case 5: return "Thursday";
-    case 6: return "Friday";
-    case 7: return "Saturday";
-    default: return "Unknown";
-  }
-}
-
-String getAbbreviatedWeekDay(int offset) {
-  String fullDay = getCurrentWeekDay(offset);
-  return fullDay.substring(0, 3);
-}
-
-int centerTextX(String text, int textSize) {
-  int textWidth = text.length() * (textSize * 6);
-  int screenWidth = display.width();
-  return (screenWidth - textWidth) / 2;
-}
-
-void updateTemps() {
-  // Yesterday's Temperature
-  display.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
-  display.setTextSize(2);
-  if (yesterdayTemp.toInt() <= -10) {
-    display.setCursor(32 - 20, 265);
-  } else if (yesterdayTemp.toInt() >= 0 && yesterdayTemp.toInt() <= 9) {
-    display.setCursor(32 + 12, 265);
-  } else {
-    display.setCursor(32, 265);
-  }
-  display.print(yesterdayTemp);
-  display.setTextSize(1);
-  display.setCursor(32+26, 260);
-  display.print("o");
-  display.setTextColor(ILI9341_LIGHTGREY, ILI9341_BLACK);
-  display.setCursor(32+26, 272);
-  display.print("F");
-
-  // Today's Temperature 
-  display.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
-  display.setTextSize(2);
-  if (todayTemp.toInt() <= -10) {
-    display.setCursor(107 - 20, 265);
-  } else if (todayTemp.toInt() >= 0 && todayTemp.toInt() <= 9) {
-    display.setCursor(107 + 12, 265);
-  } else {
-    display.setCursor(107, 265);
-  }
-  display.print(todayTemp);
-  display.setTextSize(1);
-  display.setCursor(107+26, 260);
-  display.print("o");
-  display.setTextColor(ILI9341_LIGHTGREY, ILI9341_BLACK);
-  display.setCursor(107+26, 272);
-  display.print("F");
-
-  // Tomorrow's Temperature
-  display.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
-  display.setTextSize(2);
-  if (tomorrowTemp.toInt() <= -10) {
-    display.setCursor(182 - 20, 265);
-  } else if (tomorrowTemp.toInt() >= 0 && tomorrowTemp.toInt() <= 9) {
-    display.setCursor(182 + 12, 265);
-  } else {
-    display.setCursor(182, 265);
-  }
-  display.print(tomorrowTemp);
-  display.setTextSize(1);
-  display.setCursor(182+26, 260);
-  display.print("o");
-  display.setTextColor(ILI9341_LIGHTGREY, ILI9341_BLACK);
-  display.setCursor(182+26, 272);
-  display.print("F");
-}
-
-uint8_t* getWeatherCode(String weatherCode) {
-  if (weatherCode == "1000" || weatherCode == "1100") return sunny;
-  if (weatherCode == "1101" || weatherCode == "1102" || weatherCode == "1001") return cloudy;
-  if (weatherCode == "2000" || weatherCode == "2100") return fog;
-  if (weatherCode == "4000" || weatherCode == "4001" || weatherCode == "4200" || weatherCode == "4201") return rain;
-  if (weatherCode == "5000" || weatherCode == "5001" || weatherCode == "5100" || weatherCode == "5101" || weatherCode == "6000" || weatherCode == "6001" || weatherCode == "6200" || weatherCode == "6201" || weatherCode == "7000" || weatherCode == "7101" || weatherCode == "7102") return snow;
-  if (weatherCode == "8000") return thunderstorm;
-  return blank;
 }
